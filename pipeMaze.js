f29f2be1a7834701359c218e145f4ba43acb1ce8
Kholby Lawson <klawson@dataperk.com>
2023-12-12 20:13:22 -0600

    Finished day 10!!
    The shoelace algorithm...neat.

--- pipeMaze.js
+++ pipeMaze.js
@@ -19,10 +19,16 @@ var start = null;
 	// so we go off to the north and east.
 	var next1 = { x: start[1], y: start[0], direction: Pipe.NORTH };
 	var next2 = { x: start[1], y: start[0], direction: Pipe.EAST };
+	// Example input
+	// var next1 = { x: start[1], y: start[0], direction: Pipe.SOUTH };
+	// var next2 = { x: start[1], y: start[0], direction: Pipe.WEST };
 	var steps = 1;
+	var sum = 0;
 	while( true ) {
+		last = next2;
 		next1 = sketch[next1.y][next1.x].passThrough(next1.x, next1.y, next1.direction);
 		next2 = sketch[next2.y][next2.x].passThrough(next2.x, next2.y, next2.direction);
+		sum += ((last.x+1) * (next2.y+1)) - ((next2.x+1) * (last.y+1));
 		if( next1.x === next2.x && next1.y === next2.y ) {
 			sketch[next1.y][next1.x].inTheLoop = true;
 			break;
@@ -31,45 +37,20 @@ var start = null;
 	}
 	console.log('Part 1: the furthest node from start is', steps, 'steps.');
 
-	outer: for( let i=1; i<sketch.length - 1; i++ ) {
-		for( let j=1; j<sketch[i].length - 1; j++ ) {
-			// Squares to skip in the first part (before we move away from the edges)
-			// - squares that are part of the loop
-			// - squares below null squares (ground)
-			// - squares to the right of null squares (ground)
-			// - squares below non-loop pipes
-			// - squares to the right of non-loop pipes
-			// - squares diagonally adjacent to ground or non-loop pipes
-			//   (above and to the left or right)
-			if( (sketch[i][j] !== null && sketch[i][j].inTheLoop) || 
-				sketch[i-1][j] === null || sketch[i][j-1] === null 
-				|| sketch[i-1][j-1] === null || sketch[i-1][j+1] === null
-				|| ! sketch[i-1][j-1].inTheLoop || ! sketch[i-1][j+1].inTheLoop
-				|| ! sketch[i-1][j].inTheLoop || ! sketch[i][j-1].inTheLoop ) {
-				if( sketch[i][j] !== null ) {
-					sketch[i][j].isOutside = true;
-				}
-				continue;
-			}
-			break outer;
-			// console.log(i, j, sketch[i][j]); // This is the first potential inside square.
-			// I am starting to form a plan.
-			// 1. Go around the current square and try to squeeze between, recursively.
-			// 2. If we can squeeze out anywhere, keep looping.
-			// 3. If we can't squeeze out, check next squares in this order:
-			//     i.  (x+1, y)
-			//     ii. (x-1, y+1)
-			// 4. Do recursive things!
-			// 5. When we move to a new row, move as far left as possible.
-			// 6. Try to squeeze through at every step of the process.
-			// 7. This should create a recursive stack at the first actual 
-			//    inside square that covers every other inside square.
-			if( canEscapeFrom( sketch, i, j, true ) ) {
-				// continue;
-			}
+	while( true ) {
+		last = next2;
+		next2 = sketch[next2.y][next2.x].passThrough(next2.x, next2.y, next2.direction);
+		sum += ((last.x+1) * (next2.y+1)) - ((next2.x+1) * (last.y+1));
+		if( next2.x === start[1] && next2.y === start[0] ) {
+			break;
 		}
 	}
+
 	print( sketch );
+	sum = (Math.abs(sum) / 2)-steps+1;
+
+	console.log('Part 2: there are', sum, ' squares inside the loop.');
+
 })();
 
 function parseLine( line, row ) {
@@ -108,19 +89,6 @@ function parseLine( line, row ) {
 	return pipes;
 }
 
-function canEscapeFrom( sketch, row, col, fullSearch = false ) {
-	// Try to go out the northwest corner horizontally
-	Pipe.canSqueezeByHorizontally( sketch[ row ][ col - 1 ] );
-	// Try to go out the northwest corner vertically
-	// Try to go out the northeast corner vertically
-	// Try to go out the northeast corner horizontally
-	// Try to go out the southeast corner horizontally
-	// Try to go out the southeast corner vertically
-	// Try to go out the southwest corner vertically
-	// Try to go out the southwest corner horizontally
-	return true;
-}
-
 function print( sketch ) {
 	for( let i=0; i<sketch.length; i++ ) {
 		let row = '';
@@ -149,10 +117,7 @@ class Pipe {
 		y: -1,
 		direction: false
 	};
-	static HORIZONTAL = 4;
-	static VERTICAL = 5;
 	inTheLoop = false;
-	isOutside = false;
 	symbol = 'S';
 
 	passThrough( x, y, direction ) {
@@ -167,30 +132,6 @@ class Pipe {
 			case Pipe.NORTH:
 				return { x, y: y-1, direction };
 		}
-	}
-
-	static canSqueezeBy( pipe, direction ) {
-		if( direction === Pipe.HORIZONTAL ) {
-			// Can we go over the top of this pipe?
-			if( pipe instanceof VerticalPipe || pipe instanceof LPipe || pipe instanceof JPipe ) {
-				return false;
-			}
-			return true;
-		} else {
-			// Can we go by to the right of this pipe?
-			if( pipe instanceof HorizontalPipe || pipe instanceof LPipe || pipe instanceof FPipe ) {
-				return false;
-			}
-			return true;
-		}
-	}
-
-	static canSqueezeByHorizontally( pipe ) {
-		return Pipe.canSqueezeBy( pipe, Pipe.HORIZONTAL );
-	}
-
-	static canSqueezeByVertically( pipe ) {
-		return Pipe.canSqueezeBy( pipe, Pipe.VERTICAL );
 	}
 }
 
